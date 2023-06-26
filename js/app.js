d3e64edf8e6ccdff5eca4373eda168a1c9d0e7cb
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-06-26 15:33:00 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1272,35 +1272,9 @@
         if (result) return navLang;
     }
     console.log("navigator.language", checkBrowserLang());
-    let sliderLeftBtn = document.querySelector("#slider-left");
-    let left = 0;
-    let slider = document.querySelector("#slider");
-    let startX;
-    let endX;
-    sliderLeftBtn.addEventListener("click", (function() {
-        left -= 400;
-        if (left < -800) left = 0;
-        slider.style.left = left + "px";
-    }));
-    slider.addEventListener("touchstart", (e => {
-        console.log("touchstart", e.touches[0].clientX);
-        startX = e.touches[0].clientX;
-    }));
-    slider.addEventListener("touchmove", (e => {
-        console.log("touchmovet", e.touches[0].clientX);
-        endX = e.touches[0].clientX;
-    }));
-    slider.addEventListener("touchend", (e => {
-        if (startX > endX) {
-            left -= 300;
-            if (left < -890) left = 0;
-            slider.style.left = left + "px";
-        } else {
-            left += 300;
-            if (left > 0) left = 0;
-            slider.style.left = left + "px";
-        }
-    }));
+
+
+   
     spollers();
     tabs();
 })();
